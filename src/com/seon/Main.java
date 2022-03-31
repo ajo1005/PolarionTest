386d77faa53384c79c43ed807deaf508d5e77e73
ajo1005 <37503825+ajo1005@users.noreply.github.com>
2022-03-31 15:43:35 +0900

    ``11

--- src/com/seon/Main.java
+++ src/com/seon/Main.java
@@ -3,12 +3,11 @@ package com.seon;
 public class Main {
 //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
 	/**
-	 * @wi TEST-391 :changes
+	 * @wi TEST1/TEST-391 changes
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello polarion");
 	}
 
 }
