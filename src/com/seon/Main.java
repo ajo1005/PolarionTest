d510e7d528d9f124d1e9c5971aad97ca218458a1
ajo1005 <37503825+ajo1005@users.noreply.github.com>
2022-03-31 15:41:11 +0900

    change

--- src/com/seon/Main.java
+++ src/com/seon/Main.java
@@ -2,6 +2,10 @@ package com.seon;
 
 public class Main {
 //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
+	/**
+	 * @wi TEST-391 :changes
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
